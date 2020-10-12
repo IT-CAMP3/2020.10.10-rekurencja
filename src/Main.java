66bc69b7990a9ecf6a3e8986ec15ad150d560255
Mateusz <bmatteo@interia.pl>
2020-10-12 11:18:31 +0200

    Init

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+public class Main {
+    public static void main(String[] args) {
+        //Main.a();
+        int wynik = silnia(5);
+        System.out.println(wynik);
+    }
+
+    public static void a() {
+        System.out.println("Metoda a");
+        b();
+    }
+
+    public static void b() {
+        System.out.println("Metoda b");
+        c();
+    }
+
+    public static void c() {
+        System.out.println("Metoda c");
+        d();
+    }
+
+    public static void d() {
+        System.out.println("Metoda d");
+        a();
+    }
+
+    public static int silnia(int n) {
+        if(n == 1) {
+            return 1;
+        } else {
+            return n * silnia(n-1);
+        }
+    }
+}
